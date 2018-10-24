3f2b3ed431463beb460374bb6e9296a6c56c8ea4
bryanhoffman <bryanhoffman1@gmail.com>
2018-10-24 03:10:13 +0000

    added index and store to message controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class MessageController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        $messages = Message::all();
+
+        return view('home', [
+            'messages' => $messages,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->user()->tasks()->create([
+             'message' => $request->message,
+        ]);
+
+        return redirect('/home');
     }
 }
